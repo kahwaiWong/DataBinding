2bfc8324b5ea287a8590665b851eb63c47b3730c
kahwaiWong <kahwai.blue@gmail.com>
2019-11-26 17:13:27 +0800

    initial commit

---
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    //Declare an instance of object
+    private val myContact: Contact = Contact("Wong", "011-11231145")
+    //lateinit = Late initialise (initial value to it later on)
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Binding the UI to the program variable
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.contact = myContact
+
+        binding.buttonDone.setOnClickListener{
+            changeContact(it)
+        }
+    }
+
+
+    private fun changeContact(it: View?){
+        binding.apply {
+            contact?.name = editTextName.text.toString()
+            contact?.phone = editTextPhone.text.toString()
+            this.invalidateAll() //refresh the UI
+        }
+        // binding.contact?.name = editTextName.text.toString()
+    }
+}
